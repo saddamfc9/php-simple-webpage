c7f56d42785367f67af3d33050df89320a2c735a
Saddy <125885272+saddamfc9@users.noreply.github.com>
2024-12-17 00:34:18 +0530

    changed2

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
 <?php
 
 echo "Hello Brothers!";
 echo "We are learning Azure";
 echo "Good to see you everyone here";
 ?>
+
+</body>
+</html>
